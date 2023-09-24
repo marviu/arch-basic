d97b33d09e87af3cd040ece88c6316e44fc55838
Marvin Moor <marvinmoor@mailbox.org>
2023-09-24 12:08:27 +0200

    Introduce Bootloader, os-prober and generalise uncomment_locale
    commit 7dd4a70b30afc0fdbe8b7602b9041894d3003531
    Author: Marvin Moor <marvinmoor@mailbox.org>
    Date:   Sun Sep 24 11:55:19 2023 +0200
    
        styling
    
    commit ecc6c4eb5cecccc392ab3a577a197e5191a0e364
    Author: Marvin Moor <marvinmoor@mailbox.org>
    Date:   Sun Sep 24 11:52:24 2023 +0200
    
        Add os-prober
    
    commit a84bd22a055c3bcb1f34b2e047c868a6fe2c2238
    Author: Marvin Moor <marvinmoor@mailbox.org>
    Date:   Sun Sep 24 11:30:56 2023 +0200
    
        Enable Bootloader
    
    commit 79f99dae9ad25772b83a93d92761918012487160
    Author: Marvin Moor <marvinmoor@mailbox.org>
    Date:   Sun Sep 24 11:00:25 2023 +0200
    
        Install GRUB, and efibootmgr (writes boot entries to NVRAM)
    
        Solved merge conflicts with base.sh after pull with rebase true

--- base.sh
+++ base.sh
@@ -8,20 +8,23 @@ h="bash $script_dir/helper"
 
 ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 hwclock --systohc
-$h/uncomment_locale "en_CA.UTF-8 UTF-8"
-$h/uncomment_locale "de_DE.UTF-8 UTF-8"
+$h/uncomment_line /etc/locale.gen "en_CA.UTF-8 UTF-8"
+$h/uncomment_line /etc/locale.gen "de_DE.UTF-8 UTF-8"
 locale-gen
 $h/findOrAdd_line /etc/locale.conf "LANG=en_CA.UTF-8"
 $h/findOrAdd_line /etc/vconsole.conf "KEYMAP=de-latin1"
 echo "aoba" > /etc/hostname
 $h/check_root_pw
 
-pacman -S networkmanager
+pacman -S --needed grub efibootmgr os-prober networkmanager
 
 # pacman -S --noconfirm xf86-video-amdgpu
 
-#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-#grub-mkconfig -o /boot/grub/grub.cfg
+# Comment this line and remove os-prober package for non-dualboot
+$h/uncomment_line /etc/default/grub "GRUB_DISABLE_OS_PROBER=false"
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
 #systemctl enable bluetooth
