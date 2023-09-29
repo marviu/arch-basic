8a29c5fd485880b0fe46ddc26aada0e54d4aae72
Marvin Moor <marvinmoor@mailbox.org>
2023-09-29 23:38:38 +0200

    Wayland and Pipewire drivers

--- gnome.sh
+++ gnome.sh
@@ -4,8 +4,10 @@
 sudo timedatectl set-ntp true
 sudo hwclock --systohc
 
-#sudo reflector -c Switzerland -a 12 --sort rate --save /etc/pacman.d/mirrorlist
-#sudo pacman -Sy
+# Hardware acceleration packages (AMD) (
+
+
+sudo pacman -S wayland xorg-wayland pipewire lib32-pipewire wireplumber pipewire-audio pipewire-alsa pipewire-pulse pipewire-jack lib32-pipewire-jack foot librewolf discord
 
 #sudo firewall-cmd --add-port=1025-65535/tcp --permanent
 #sudo firewall-cmd --add-port=1025-65535/udp --permanent
@@ -13,7 +15,7 @@ sudo hwclock --systohc
 ## sudo virsh net-autostart default
 
 
-#sudo pacman -S gnome-shell gdm gnome-control-center gnome-tweaks
+sudo pacman -S gnome-shell gdm gnome-control-center gnome-tweaks
 #sudo pacman -S --noconfirm xorg gdm gnome gnome-extra firefox gnome-tweaks simplescreenrecorder arc-gtk-theme arc-icon-theme obs-studio vlc dina-font tamsyn-font bdf-unifont ttf-bitstream-vera ttf-croscore ttf-dejavu ttf-droid gnu-free-fonts ttf-ibm-plex ttf-liberation ttf-linux-libertine noto-fonts ttf-roboto tex-gyre-fonts ttf-ubuntu-font-family ttf-anonymous-pro ttf-cascadia-code ttf-fantasque-sans-mono ttf-fira-mono ttf-hack ttf-fira-code ttf-inconsolata ttf-jetbrains-mono ttf-monofur adobe-source-code-pro-fonts cantarell-fonts inter-font ttf-opensans gentium-plus-font ttf-junicode adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
 
 # sudo flatpak install -y spotify
